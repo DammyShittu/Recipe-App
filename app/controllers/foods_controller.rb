9d53dc04fa4b5a138f339da3dee1f9176ceedb50
kalunge <muthomititus+github@gmail.com>
2021-12-20 16:57:39 +0100

    Add food create action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,11 +4,31 @@ class FoodsController < ApplicationController
   end
 
   def show
+    @food = Food.find_by(id: params[:id])
   end
 
-  def create
+
+  def new
+    @food = Food.create(food_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'food created succeffuly'
+          redirect_to foods_path
+        else
+          flash[:error] = 'food not created try again'
+          render :new
+        end
+      end
+    end
   end
 
   def destroy
   end
+
+  private
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price)
+    end
+  
 end
