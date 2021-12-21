c4d07e2ac6e57615204907035531b7830228e473
kalunge <muthomititus+github@gmail.com>
2021-12-21 14:12:52 +0100

    Implement requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :foods
   devise_scope :user do 
     authenticated :user do 
       root 'users#index', as: :authenticated_root 
@@ -9,5 +8,5 @@ Rails.application.routes.draw do
       root 'devise/sessions#new', as: :unauthenticated_root 
     end 
   end 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :foods, only: [:index, :new, :create, :show, :destroy]
 end
