6b9865e85793b7db583446a70c40f7129c5e2149
kalunge <muthomititus+github@gmail.com>
2021-12-22 10:50:14 +0100

    Add food model tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'valid if name is present' do
+    @user = User.create!(name: "Dammy", email: "dammy@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Chapo", measurement_unit: "grams", price: 150)
+    expect(@food).to be_valid
+  end
+
+  it 'invalid if name absent' do
+    @user = User.create!(name: "shittu", email: "shittu@gmail.com", password: "qwerty")
+    @food = @user.foods.create(measurement_unit: "grams", price: 50)
+    expect(@food).to_not be_valid
+  end
+
+  it 'Must be associated with a user' do
+    @food = Food.create(name: "Muthokoi", measurement_unit: "grams", price: 500)
+    expect(@food).to_not be_valid
+  end
+
+  it 'valid if User present' do
+    @user = User.create!(name: "kalunge", email: "kalunge@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Karanga", measurement_unit: "grams", price: 50)
+    expect(@food).to be_valid
+  end
+
+    it 'valid if name is present' do
+    @user = User.create!(name: "Dammy", email: "dammy@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Chapo", measurement_unit: "grams", price: 150)
+    expect(@food).to be_valid
+  end
+
+  it 'Must have a price' do
+    @user = User.create!(name: "shittu", email: "shittu@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Rice", measurement_unit: "grams", price: 50)
+    expect(@food).to be_valid
+  end
+
+  it 'invalid if price is absent' do
+    @user = User.create!(name: "shittu", email: "shittu@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Rice", measurement_unit: "grams")
+    expect(@food).to_not be_valid
+  end
+
+  it 'Must have a measurement_unit' do
+    @user = User.create!(name: "shittu", email: "shittu@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Rice", measurement_unit: "grams", price: 50)
+    expect(@food).to be_valid
+  end
+
+  it 'invalid if price is absent' do
+    @user = User.create!(name: "shittu", email: "shittu@gmail.com", password: "qwerty")
+    @food = @user.foods.create(name: "Rice", price: 570)
+    expect(@food).to_not be_valid
+  end
 end
