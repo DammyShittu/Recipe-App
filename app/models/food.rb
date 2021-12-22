31fc329367aa55fff5ec282a1d57c689117fc4a2
kalunge <muthomititus+github@gmail.com>
2021-12-22 10:44:57 +0100

    Add model validations for User and Food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, presence: { message: 'You must fill in the food name' }
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: { message: 'You must fill in your name' }
+  validates :email, presence: { message: 'email field cannot be left blank' }
+  validates :password, presence: { message: 'password field cannot be left blank' }
+
   has_many :foods, dependent: :destroy
   has_many :recipes, foreign_key: 'user_id', dependent: :destroy
 end
