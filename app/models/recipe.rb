cf40a38450d278ca33093cf04056c42ea8425cab
kalunge <muthomititus+github@gmail.com>
2021-12-23 08:15:29 +0100

    Resolve merge conflicts in RecipeFood

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,12 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_foods, dependent: :destroy
   belongs_to :user
+
+  def total
+    recipe_foods.joins(:recipe, :food).sum('price * quantity')
+  end
+
+  def total_items
+    recipe_foods.sum(:quantity)
+  end
 end
