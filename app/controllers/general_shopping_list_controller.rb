773f100e198efbcc4ca24245037d0426193294e6
kalunge <muthomititus+github@gmail.com>
2021-12-23 09:05:52 +0100

    Resolve rubocop errors

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -3,7 +3,7 @@ class GeneralShoppingListController < ApplicationController
     @recipes = current_user.recipes
     @foods = Food.joins(:recipe_foods).where(user_id: current_user.id)
     @total = 0
-    @foods.each do | food |
+    @foods.each do |food|
       @total += food.price
     end
   end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,7 +3,6 @@ class RecipeFood < ApplicationRecord
   belongs_to :food
 
   validates :quantity, presence: { message: 'You must fill in the food name' }
-  
 
   def calc_value(quantity, price)
     quantity * price
